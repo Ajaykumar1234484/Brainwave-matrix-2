60568253da5f9fa445416208a6a2be94fd134962
Ajaykumar1234484 <147429842+Ajaykumar1234484@users.noreply.github.com>
2024-12-30 13:02:08 +0530

    Add files via upload

---
+++ aj.js
@@ -0,0 +1,8 @@
+<script>
+// JavaScript Functionality
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Item added to cart!');
+    });
+});
+</script>
